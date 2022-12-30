5b98618ff4abe85506c606652c3aae70d99eaede
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-12-31 05:01:50 +0530

    navbar is added

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import './Navbar.css'
+import Logo from '../../Assets/imgs/i2plogo2.png'
+import svg from '../../Assets/imgs/gh_svg.svg'
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <span className="navbar-brand" ><img className="nav-logo" alt="Infix2Postfix_logo" src={Logo} /></span>
+            <a className="gh-link" href="https://github.com/Sayan-Maity"><img src={svg} className="gh-svg" alt="github-icon" /></a>
+            <span className="navbar-link"><a className="gh-link" href="https://github.com/Sayan-Maity">Star me on Github</a></span>
+        </div>
+    )
+}
+
+export default Navbar
